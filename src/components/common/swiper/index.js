95d3519de6820ec26b14c7d965a26b7bf0f55c5f
Mr.G <941653940@qq.com>
2020-10-23 00:08:57 +0800

    10/23 0.08

--- src/components/common/swiper/index.js
+++ src/components/common/swiper/index.js
@@ -1,7 +1,9 @@
 import Swiper from 'components/common/swiper/Swiper'
+import test from 'components/common/swiper/Swiper'
 import SwiperItem from 'components/common/swiper/SwiperItem'
 
 export {
     Swiper,
-    SwiperItem
+    SwiperItem,
+    test
 }
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,14 +2,14 @@
   <div id="home">
     <nav-bar class="home-nav">
       <div slot="center">购物街</div>
-      <swiper>
-        <swiper-item v-for="(item, index) in banners" :key="index">
-          <a :href="item.link">
-            <img :src="item.image" alt="" />
-          </a>
-        </swiper-item>
-      </swiper>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="(item, index) in banners" :key="index">
+        <a :href="item.link">
+          <img :src="item.image" alt="" />
+        </a>
+      </swiper-item>
+    </swiper>
     <!-- <h3>首页</h3> -->
   </div>
 </template>
@@ -17,11 +17,7 @@
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import { getHomeMultidata } from "network/home";
-// import { Swiper, SwiperItem } from "components/common/swiper/index";
-// import Swiper from "components/common/swiper/Swiper";
-// import SwiperItem from "components/common/swiper/SwiperItem";
-import Swiper from 'components/common/swiper/Swiper.vue'
-import SwiperItem from 'components/common/swiper/SwiperItem.vue'
+import { Swiper, SwiperItem } from "components/common/swiper/index";
 
 export default {
   name: "Home",
