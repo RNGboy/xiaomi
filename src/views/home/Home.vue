0f34db85df8a904fae51c03c2230b99e0421fb55
Mr.G <941653940@qq.com>
2020-10-22 23:14:14 +0800

    10.22

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,17 +1,55 @@
 <template>
   <div id="home">
-    <nav-bar></nav-bar>
+    <nav-bar class="home-nav">
+      <div slot="center">购物街</div>
+      <swiper>
+        <swiper-item v-for="(item, index) in banners" :key="index">
+          <a :href="item.link">
+            <img :src="item.image" alt="" />
+          </a>
+        </swiper-item>
+      </swiper>
+    </nav-bar>
     <!-- <h3>首页</h3> -->
-
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
+import { getHomeMultidata } from "network/home";
+// import { Swiper, SwiperItem } from "components/common/swiper/index";
+// import Swiper from "components/common/swiper/Swiper";
+// import SwiperItem from "components/common/swiper/SwiperItem";
+import Swiper from 'components/common/swiper/Swiper.vue'
+import SwiperItem from 'components/common/swiper/SwiperItem.vue'
+
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends: [],
+    };
+  },
   components: {
     NavBar,
+    Swiper,
+    SwiperItem,
+  },
+  created() {
+    // 1.请求多个数据
+    getHomeMultidata().then((res) => {
+      // console.log(res)
+      this.banners = res.data.banner.list;
+      this.recommends = res.data.recommend.list;
+    });
   },
 };
 </script>
+
+<style lang="css" scoped>
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
+</style>
